547ddf6d4c5e5e03d3e130427279b967a604cb69
andrey-dihtiarov <kama4ok222@gmail.com>
2021-05-21 16:34:08 +0300

    project-skeleton: general.js renamed to index.js, routes constants added

--- src/constants/index.js
+++ src/constants/index.js
@@ -25,3 +25,11 @@ export const FORMS = [
     form: 'Capabilities',
   },
 ];
+
+export const ROUTES = {
+  home: '/',
+  user: '/users/:id',
+  users: '/users',
+  newUser: '/users/new',
+  editUser: '/users/:id/edit',
+};
