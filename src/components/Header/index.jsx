80fdbe11931564f38214e361520e731e4e426657
andrey-dihtiarov <kama4ok222@gmail.com>
2021-05-24 11:37:58 +0300

    account-form: Header component - new header styles added - header navigation added

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,3 +1,36 @@
-const Header = () => <div>Header</div>;
+import { useHistory } from 'react-router-dom';
+
+import { ROUTES } from '../../constants';
+
+import LinkButton from '../buttons/LinkButton';
+import { UserIcon, UsersIcon } from '../icons';
+
+import classes from './Header.module.scss';
+
+const Header = () => {
+  const history = useHistory();
+
+  const onUserClick = () => history.push(`${ROUTES.newUser}${ROUTES.accountForm}`);
+  const onUsersClick = () => history.push(ROUTES.users);
+  const onLogoClick = () => history.push(ROUTES.home);
+
+  return (
+    <div className={classes.header}>
+      <div className={classes.container}>
+        <div className={classes.logo} onClick={onLogoClick} role="presentation">
+          Remake
+        </div>
+        <div className={classes.nav}>
+          <LinkButton icon={UserIcon} onClick={onUserClick} className={classes.nav_button}>
+            Add new user
+          </LinkButton>
+          <LinkButton icon={UsersIcon} onClick={onUsersClick} className={classes.nav_button}>
+            List of users
+          </LinkButton>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
