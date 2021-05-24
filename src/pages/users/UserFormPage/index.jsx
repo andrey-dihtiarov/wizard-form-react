4b7e3673820e2f2929c60b747d5583a249cfce55
andrey-dihtiarov <kama4ok222@gmail.com>
2021-05-24 11:46:13 +0300

    account-form: UserFormPage component - new styles added - "formik" form added - basic forms validation with "yup" provided

--- src/pages/users/UserFormPage/index.jsx
+++ src/pages/users/UserFormPage/index.jsx
@@ -1,10 +1,14 @@
 import { Route, Switch, useRouteMatch } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { Form as FormikForm, Formik } from 'formik';
+import * as Yup from 'yup';
 
 import AccountForm from '../../../components/forms/AccountForm';
 import ProfileForm from '../../../components/forms/ProfileForm';
 import ContactsForm from '../../../components/forms/ContactsForm';
 import CapabilitiesForm from '../../../components/forms/CapabilitiesForm';
+
+import classes from './UserFormPage.module.scss';
 
 export const FORMS = [
   {
@@ -25,16 +29,36 @@ export const FORMS = [
   },
 ];
 
+const validationSchema = Yup.object().shape({
+  userName: Yup.string()
+    .min(2, 'User Name is too short')
+    .max(70, 'User Name is too long')
+    .required('Required'),
+  password: Yup.string()
+    .required('Please, enter a password')
+    .min(8, 'Password should be at least 8 characters long'),
+  repeatPassword: Yup.string()
+    .required('Please, repeat entered password')
+    .oneOf([Yup.ref('password'), null], 'Passwords must match'),
+});
 const UserFormPage = ({ isEditing }) => {
   const { path } = useRouteMatch();
   return (
-    <div>
-      {isEditing ? 'Edit User Page' : 'New User Page'}
-      <Switch>
-        {FORMS.map(({ component: Form, slug }) => (
-          <Route key={slug} exact path={`${path}/${slug}`} component={Form} />
-        ))}
-      </Switch>
+    <div className={classes.wrapper}>
+      {isEditing ? 'Edit User Page' : 'New User Pages'}
+      <Formik
+        initialValues={{ userName: '', password: '', repeatPassword: '' }}
+        onSubmit={() => {}}
+        validationSchema={validationSchema}
+      >
+        <FormikForm>
+          <Switch>
+            {FORMS.map(({ component: Form, slug }) => (
+              <Route key={slug} exact path={`${path}/${slug}`} component={Form} />
+            ))}
+          </Switch>
+        </FormikForm>
+      </Formik>
     </div>
   );
 };
