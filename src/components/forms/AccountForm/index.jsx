a480566e006dc5074ce258dc30247168b78423f3
andrey-dihtiarov <kama4ok222@gmail.com>
2021-05-24 11:43:23 +0300

    account-form: AccountForm component - AccountForm basic styles added - All required AccountForm fields added - added fields are "formik" Fields - Forward button added

--- src/components/forms/AccountForm/index.jsx
+++ src/components/forms/AccountForm/index.jsx
@@ -1,3 +1,37 @@
-const AccountForm = () => <div>Account Form</div>;
+import { useState } from 'react';
+import { Field } from 'formik';
+
+import TextInput from '../../inputs/TextInput';
+import PasswordInput from '../../inputs/PasswordInput';
+import FlatButton from '../../buttons/FlatButton';
+import Avatar from '../../Avatar';
+import ImageUploader from '../../ImageUploader';
+
+import classes from './AccountForm.module.scss';
+
+const AccountForm = () => {
+  const [imageSrc, setImageSrc] = useState();
+
+  return (
+    <div className={classes.form}>
+      <div className={classes.formContainer}>
+        <div className={classes.avatarContainer}>
+          <Avatar image={imageSrc} />
+          <ImageUploader className={classes.avatarButton} onChange={setImageSrc} />
+        </div>
+        <div className={classes.fieldsContainer}>
+          <div>
+            <Field name="userName" label="User Name" component={TextInput} />
+            <Field name="password" label="Password" component={PasswordInput} />
+            <Field name="repeatPassword" label="Repeat Password" component={PasswordInput} />
+          </div>
+          <div className={classes.buttonContainer}>
+            <FlatButton>Forward</FlatButton>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default AccountForm;
