91a9c2108e8ff285a495813aaabd0d6ea55214a6
Andrey Dihtiariov <andrey.d@giantleaplab.com>
2021-05-26 00:29:51 +0300

    profile-account: Birth date input field added to ProfileForm

--- src/components/forms/ProfileForm/index.jsx
+++ src/components/forms/ProfileForm/index.jsx
@@ -1,3 +1,13 @@
-const ProfileForm = () => <div>Profile Form</div>;
+import { Field } from 'formik';
+
+import DateInput from '../../inputs/DateInput';
+
+// import styles from '../AccountForm/styles.module.scss';
+
+const ProfileForm = () => (
+  <div>
+    <Field name="birthDate" label="Birth date" component={DateInput} />
+  </div>
+);
 
 export default ProfileForm;
