1312f8374475de4059ba5148495a960d02f44e0c
andrey-dihtiarov <kama4ok222@gmail.com>
2021-05-28 18:27:23 +0300

    step-wizard: PropTypes check added to CapabilitiesForm component

--- src/components/forms/CapabilitiesForm/index.jsx
+++ src/components/forms/CapabilitiesForm/index.jsx
@@ -1,3 +1,14 @@
-const CapabilitiesForm = () => <div>Capabilities Form</div>;
+import PropTypes from 'prop-types';
+// import FormikForm from '../../FormikForm';
+
+const CapabilitiesForm = ({ children }) => <div>{children}</div>;
+
+CapabilitiesForm.propTypes = {
+  children: PropTypes.node,
+};
+
+CapabilitiesForm.defaultProp = {
+  children: null,
+};
 
 export default CapabilitiesForm;
