4957a60c6cbba63f75ab305c9332e801d8786bdc
andrey-dihtiarov <kama4ok222@gmail.com>
2021-05-28 18:27:02 +0300

    step-wizard: PropTypes check added to ContactsForm component

--- src/components/forms/ContactsForm/index.jsx
+++ src/components/forms/ContactsForm/index.jsx
@@ -1,3 +1,14 @@
-const ContactsForm = () => <div>Contact Form</div>;
+import PropTypes from 'prop-types';
+// import FormikForm from '../../FormikForm';
+
+const ContactsForm = ({ children }) => <div>{children}</div>;
+
+ContactsForm.propTypes = {
+  children: PropTypes.node,
+};
+
+ContactsForm.defaultProp = {
+  children: null,
+};
 
 export default ContactsForm;
