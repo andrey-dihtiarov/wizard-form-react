760cc5d9a13167fe6ebbdc6e545b1804193a9367
andrey-dihtiarov <kama4ok222@gmail.com>
2021-05-28 18:33:16 +0300

    step-wizard: ProfileForm - ProfileForm wrapped in FormikForm - validationSchema added

--- src/components/forms/ProfileForm/index.jsx
+++ src/components/forms/ProfileForm/index.jsx
@@ -1,17 +1,38 @@
 import { Field } from 'formik';
+import { subYears } from 'date-fns';
+import { useSelector } from 'react-redux';
+import PropTypes from 'prop-types';
+import * as Yup from 'yup';
 
 import DateInput from '../../inputs/DateInput';
 import RadioGroup from '../../RadioGroup';
 import AddressInput from '../../inputs/AddressInput';
+import FormikForm from '../../FormikForm';
 
-const GenderRadioGroupValues = ['Male', 'Female'];
+const validationSchema = Yup.object().shape({
+  birthDate: Yup.date().max(subYears(Date.now(), 18), 'User must be older 18').required('Required'),
+  gender: Yup.string().oneOf(['Male', 'Female']).required('Required'),
+});
 
-const ProfileForm = () => (
-  <div>
-    <Field name="birthDate" label="Birth date" component={DateInput} />
-    <RadioGroup values={GenderRadioGroupValues} name="gender" />
-    <Field name="address" label="Address" component={AddressInput} />
-  </div>
-);
+const ProfileForm = ({ children, onSubmit }) => {
+  const { birthDate, gender, address } = useSelector((state) => state.user);
+  const initValues = { birthDate, gender, address };
+  return (
+    <FormikForm initialValues={initValues} validationSchema={validationSchema} submit={onSubmit}>
+      <Field name="birthDate" label="Birth date" component={DateInput} />
+      <RadioGroup values={['Male', 'Female']} name="gender" />
+      <Field name="address" label="Address" component={AddressInput} />
+      {children}
+    </FormikForm>
+  );
+};
+
+ProfileForm.propTypes = {
+  children: PropTypes.node,
+};
+
+ProfileForm.defaultProp = {
+  children: null,
+};
 
 export default ProfileForm;
