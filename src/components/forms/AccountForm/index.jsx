624b4a46fb29b7df34e3d27126a6b152b49c07da
andrey-dihtiarov <kama4ok222@gmail.com>
2021-05-28 18:33:42 +0300

    step-wizard: AccountForm - AccountForm wrapped in FormikForm - validationSchema added

--- src/components/forms/AccountForm/index.jsx
+++ src/components/forms/AccountForm/index.jsx
@@ -1,18 +1,56 @@
 import { useState } from 'react';
 import { Field } from 'formik';
+import { useSelector } from 'react-redux';
+import PropTypes from 'prop-types';
+import * as Yup from 'yup';
 
 import TextInput from '../../inputs/TextInput';
 import PasswordInput from '../../inputs/PasswordInput';
 import Avatar from '../../Avatar';
 import ImageUploader from '../../ImageUploader';
+import FormikForm from '../../FormikForm';
 
 import styles from './styles.module.scss';
 
-const AccountForm = () => {
-  const [imageSrc, setImageSrc] = useState();
+const FILE_SIZE = 1024 * 1024;
 
+const SUPPORTED_FORMATS = new Set([
+  'image/jpg',
+  'image/jpeg',
+  'image/svg',
+  'image/gif',
+  'image/png',
+]);
+
+const validationSchema = Yup.object().shape({
+  userName: Yup.string()
+    .min(2, 'User Name is too short')
+    .max(70, 'User Name is too long')
+    .required('Required'),
+  password: Yup.string()
+    .required('Please, enter a password')
+    .min(8, 'Password should be at least 8 characters long'),
+  repeatPassword: Yup.string()
+    .required('Please, repeat entered password')
+    .oneOf([Yup.ref('password'), null], 'Passwords must match'),
+  avatar: Yup.mixed()
+    .test('fileSize', 'File too large', (value) => (value ? value.size <= FILE_SIZE : true))
+    .test('fileFormat', 'Unsupported Format', (value) =>
+      value ? SUPPORTED_FORMATS.has(value.type) : true,
+    ),
+});
+
+const AccountForm = ({ children, onSubmit }) => {
+  const [imageSrc, setImageSrc] = useState();
+  const { userName, password, repeatPassword, avatar } = useSelector((state) => state.user);
+  const initValues = { userName, password, repeatPassword, avatar };
   return (
-    <div className={styles.form}>
+    <FormikForm
+      initialValues={initValues}
+      validationSchema={validationSchema}
+      submit={onSubmit}
+      className={styles.form}
+    >
       <div className={styles.formContainer}>
         <div className={styles.avatarContainer}>
           <Avatar image={imageSrc} />
@@ -31,8 +69,18 @@ const AccountForm = () => {
           </div>
         </div>
       </div>
-    </div>
+      {children}
+    </FormikForm>
   );
+};
+
+AccountForm.propTypes = {
+  children: PropTypes.node,
+  onSubmit: PropTypes.func,
+};
+
+AccountForm.defaultProps = {
+  children: null,
 };
 
 export default AccountForm;
