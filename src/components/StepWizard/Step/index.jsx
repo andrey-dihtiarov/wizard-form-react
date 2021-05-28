6a924b2673e6495453efee6e8e998741611a1e29
andrey-dihtiarov <kama4ok222@gmail.com>
2021-05-28 18:31:16 +0300

    step-wizard: Step Component - updateUser redux dispatch added - onSubmit method added - Nav component removed from Step

--- src/components/StepWizard/Step/index.jsx
+++ src/components/StepWizard/Step/index.jsx
@@ -1,45 +1,58 @@
+import React from 'react';
+import { useRouteMatch, useParams, useHistory } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
 
-import Nav from '../Nav';
 import FlatButton from '../../buttons/FlatButton';
 
 import styles from './styles.module.scss';
+import { updateUser } from '../../../store/user';
 
-const Step = ({ show, children, isFirst, isLast, slugs }) => {
-  const indexOfSlug = (slug) => slugs.findIndex((item) => item === slug);
-  const onBackClick = (slug) => slugs[indexOfSlug(slug) - 1];
-  const onNextClick = (slug) => slugs[indexOfSlug(slug) + 1];
+const Step = ({ children, isFirst, isLast, slugs }) => {
+  const { path } = useRouteMatch();
+  const { slug } = useParams();
+  const history = useHistory();
+  const dispatch = useDispatch();
+
+  const indexOfSlug = () => slugs.findIndex((item) => item === slug);
+  const onBackClick = () => history.push(path.replace(':slug', slugs[indexOfSlug(slug) - 1]));
   const onFinishClick = () => {};
+  const onSubmit = (values) => {
+    dispatch(updateUser(values));
+    history.push(path.replace(':slug', slugs[indexOfSlug(slug) + 1]));
+  };
 
   const backButton = (
-    <Nav computedSlug={onBackClick}>
-      <FlatButton variant="cancel">Back</FlatButton>
-    </Nav>
+    <FlatButton type="button" variant="cancel" onClick={onBackClick}>
+      Back
+    </FlatButton>
   );
   const nextButton = isLast ? (
-    <FlatButton variant="success" onClick={onFinishClick}>
+    <FlatButton type="button" variant="success" onClick={onFinishClick}>
       Finish
     </FlatButton>
   ) : (
-    <Nav computedSlug={onNextClick}>
-      <FlatButton variant="primary">Forward</FlatButton>
-    </Nav>
+    <FlatButton type="submit" variant="primary">
+      Forward
+    </FlatButton>
   );
 
-  return show ? (
-    <div className={styles.stepWrapper}>
-      {children}
-      <div className={styles.buttonsWrapper}>
-        <div>{!isFirst && backButton}</div>
-        <div className={styles.buttonNext}>{nextButton}</div>
-      </div>
+  const navigation = (
+    <div className={styles.buttonsWrapper}>
+      <div>{!isFirst && backButton}</div>
+      <div className={styles.buttonNext}>{nextButton}</div>
     </div>
-  ) : null;
+  );
+
+  const mappedChildren = React.Children.map(children, (child) =>
+    React.cloneElement(child, { children: navigation, onSubmit }),
+  );
+
+  return <div className={styles.stepWrapper}>{mappedChildren}</div>;
 };
 
 Step.propTypes = {
-  show: PropTypes.bool.isRequired,
-  children: PropTypes.node.isRequired,
+  children: PropTypes.element.isRequired,
   isFirst: PropTypes.bool.isRequired,
   isLast: PropTypes.bool.isRequired,
   slugs: PropTypes.array.isRequired,
