65b8da671a1332ea0cf194f81c0716618f0f493a
Andrey Dihtiariov <andrey.d@giantleaplab.com>
2021-05-29 19:15:59 +0300

    step-wizard: onForward, onBack and onSubmit props added to Step component

--- src/components/StepWizard/Step/index.jsx
+++ src/components/StepWizard/Step/index.jsx
@@ -1,24 +1,29 @@
 import React from 'react';
 import { useRouteMatch, useParams, useHistory } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
 
 import FlatButton from '../../buttons/FlatButton';
 
 import styles from './styles.module.scss';
-import { updateUser } from '../../../store/user';
 
-const Step = ({ children, isFirst, isLast, slugs }) => {
+const Step = ({ children, isFirst, isLast, slugs, onBack, onForward, onFinish }) => {
   const { path } = useRouteMatch();
   const { slug } = useParams();
   const history = useHistory();
-  const dispatch = useDispatch();
 
   const indexOfSlug = () => slugs.findIndex((item) => item === slug);
-  const onBackClick = () => history.push(path.replace(':slug', slugs[indexOfSlug(slug) - 1]));
-  const onFinishClick = () => {};
-  const onSubmit = (values) => {
-    dispatch(updateUser(values));
+
+  const onBackClick = () => {
+    onBack();
+    history.push(path.replace(':slug', slugs[indexOfSlug(slug) - 1]));
+  };
+
+  const onFinishClick = (values) => {
+    onFinish(values);
+  };
+
+  const onForwardClick = (values) => {
+    onForward(values, slug);
     history.push(path.replace(':slug', slugs[indexOfSlug(slug) + 1]));
   };
 
@@ -27,6 +32,7 @@ const Step = ({ children, isFirst, isLast, slugs }) => {
       Back
     </FlatButton>
   );
+
   const nextButton = isLast ? (
     <FlatButton type="button" variant="success" onClick={onFinishClick}>
       Finish
@@ -45,7 +51,10 @@ const Step = ({ children, isFirst, isLast, slugs }) => {
   );
 
   const mappedChildren = React.Children.map(children, (child) =>
-    React.cloneElement(child, { children: navigation, onSubmit }),
+    React.cloneElement(child, {
+      children: navigation,
+      onSubmit: isLast ? onFinishClick : onForwardClick,
+    }),
   );
 
   return <div className={styles.stepWrapper}>{mappedChildren}</div>;
@@ -56,6 +65,15 @@ Step.propTypes = {
   isFirst: PropTypes.bool.isRequired,
   isLast: PropTypes.bool.isRequired,
   slugs: PropTypes.array.isRequired,
+  onForward: PropTypes.func,
+  onBack: PropTypes.func,
+  onFinish: PropTypes.func,
+};
+
+Step.defaultProps = {
+  onForward: () => {},
+  onBack: () => {},
+  onFinish: () => {},
 };
 
 export default Step;
