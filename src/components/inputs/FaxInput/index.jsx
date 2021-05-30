901a6f19b5c58ce8f0a86021e8d144cd3ab7686b
Andrey Dihtiariov <andrey.d@giantleaplab.com>
2021-05-30 23:53:32 +0300

    contacts-form: FaxInput adjusted to use in PhoneGroup component

--- src/components/inputs/FaxInput/index.jsx
+++ src/components/inputs/FaxInput/index.jsx
@@ -1,3 +1,4 @@
+import { useMemo } from 'react';
 import PropTypes from 'prop-types';
 import InputMask from 'react-input-mask';
 
@@ -7,7 +8,27 @@ import styles from './styles.module.scss';
 
 const FaxInput = ({ form: { touched, errors, setFieldValue, setFieldTouched }, label, field }) => {
   const { name, value } = field;
-  const isError = !!(touched[name] && errors[name]);
+
+  const isError = useMemo(() => {
+    const [fieldName, index] = name.split('.');
+
+    if (index) {
+      const parsedIndex = parseInt(index, 10);
+
+      const isExists = !!(
+        touched[fieldName] &&
+        touched[fieldName][parsedIndex] &&
+        errors[fieldName] &&
+        errors[fieldName][parsedIndex]
+      );
+
+      if (isExists) {
+        return !!(touched[fieldName][parsedIndex] && errors[fieldName][parsedIndex]);
+      }
+    }
+    return !!(touched[name] && errors[name]);
+  }, [errors, name, touched]);
+
   return (
     <InputContainer field={field} label={label}>
       <InputMask
@@ -37,10 +58,16 @@ FaxInput.propTypes = {
     setFieldValue: PropTypes.func.isRequired,
     setFieldTouched: PropTypes.func.isRequired,
   }).isRequired,
+  fieldIndex: PropTypes.number,
+  phoneField: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    index: PropTypes.number.isRequired,
+  }),
 };
 
 FaxInput.defaultProps = {
   label: '',
+  fieldIndex: null,
 };
 
 export default FaxInput;
