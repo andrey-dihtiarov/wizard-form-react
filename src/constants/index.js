968ebcca904591c209b5f34ee7d7f17378887d7a
andrey-dihtiarov <kama4ok222@gmail.com>
2021-05-31 13:06:57 +0300

    capabilities-form: new constants for skills and hobbies fields added

--- src/constants/index.js
+++ src/constants/index.js
@@ -14,3 +14,34 @@ export const ICONS = {
   add: 'add',
   userFriends: 'user-friends',
 };
+
+export const SKILLS = Object.freeze([
+  'HTML',
+  'CSS',
+  'Javascript',
+  'React',
+  'Angular',
+  'jQuery',
+  'NodeJS',
+  'Python',
+  'PHP',
+  'Ruby On Rails',
+  'SQL',
+  'BackboneJS',
+  'Web Design',
+  'Project management',
+  'Git',
+  'Docker',
+  'AWS Lambda',
+  'Firebase',
+]);
+
+export const HOBBIES = Object.freeze([
+  'Music',
+  'Art',
+  'Traveling',
+  'Procrastinating',
+  'Sport',
+  'Driving',
+  'Singing',
+]);
