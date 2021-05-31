03a3a614b25fad8c3bc27ead656bb74c60d72a31
andrey-dihtiarov <kama4ok222@gmail.com>
2021-05-31 13:13:03 +0300

    capabilities-form: CapabilitiesForm - all required fields added - new styles added

--- src/components/forms/CapabilitiesForm/index.jsx
+++ src/components/forms/CapabilitiesForm/index.jsx
@@ -1,3 +1,21 @@
-const CapabilitiesForm = () => <div>Capabilities Form</div>;
+import { Field } from 'formik';
+
+import SkillsMultiselectInput from '../../inputs/SkillsMultiselectInput';
+import TextAreaInput from '../../inputs/TextAreaInput';
+import HobbiesInput from '../../inputs/HobbiesInput';
+
+import styles from './styles.module.scss';
+
+const CapabilitiesForm = () => (
+  <div className={styles.form}>
+    <div className={styles.fieldsWrapper}>
+      <Field name="skills" label="Skills" component={SkillsMultiselectInput} />
+      <Field name="additionalInfo" label="Additional Info" component={TextAreaInput} />
+    </div>
+    <div className={styles.fieldsWrapper}>
+      <Field name="myHobbies" label="My Hobbies" component={HobbiesInput} />
+    </div>
+  </div>
+);
 
 export default CapabilitiesForm;
