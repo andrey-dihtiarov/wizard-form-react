6f190f5fa5443abd02b9dd7921438c773beb3c68
andrey-dihtiarov <kama4ok222@gmail.com>
2021-06-01 13:33:26 +0300

    step-wizard: unnecessary "to" prop removed

--- src/components/buttons/LinkButton/index.jsx
+++ src/components/buttons/LinkButton/index.jsx
@@ -3,8 +3,8 @@ import { NavLink } from 'react-router-dom';
 
 import styles from './styles.module.scss';
 
-const LinkButton = ({ icon, className, children, to, ...props }) => (
-  <NavLink to={to} className={`${styles.button} ${className}`} {...props}>
+const LinkButton = ({ icon, className, children, ...rest }) => (
+  <NavLink className={`${styles.button} ${className}`} {...rest}>
     {icon && <span className={styles.buttonIcon}>{icon}</span>} {children}
   </NavLink>
 );
@@ -12,14 +12,12 @@ const LinkButton = ({ icon, className, children, to, ...props }) => (
 LinkButton.propTypes = {
   children: PropTypes.string,
   className: PropTypes.string,
-  to: PropTypes.string,
   icon: PropTypes.node,
 };
 
 LinkButton.defaultProps = {
   children: 'Link Button',
   className: '',
-  to: '',
   icon: null,
 };
 
