3563672327eba1a8b7f4b2b3be8d7ea0e87aabf9
andrey-dihtiarov <kama4ok222@gmail.com>
2021-06-02 13:37:38 +0300

    step-wizard: NavButtons component is simplified

--- src/components/NavButtons/index.jsx
+++ src/components/NavButtons/index.jsx
@@ -1,30 +1,24 @@
 import PropTypes from 'prop-types';
 import FlatButton from '../buttons/FlatButton';
 
-const NavButtons = ({ isFirst, isLast, onBack }) => {
-  const backButton = !isFirst && (
-    <FlatButton type="button" variant="cancel" onClick={onBack}>
-      Back
-    </FlatButton>
-  );
-
-  const nextButton = isLast ? (
-    <FlatButton type="submit" variant="success">
-      Finish
-    </FlatButton>
-  ) : (
-    <FlatButton type="submit" variant="primary">
-      Forward
-    </FlatButton>
-  );
-
-  return (
-    <>
-      {backButton}
-      {nextButton}
-    </>
-  );
-};
+const NavButtons = ({ isFirst, isLast, onBack }) => (
+  <>
+    {!isFirst && (
+      <FlatButton type="button" variant="cancel" onClick={onBack}>
+        Back
+      </FlatButton>
+    )}
+    {isLast ? (
+      <FlatButton type="submit" variant="success">
+        Finish
+      </FlatButton>
+    ) : (
+      <FlatButton type="submit" variant="primary">
+        Forward
+      </FlatButton>
+    )}
+  </>
+);
 
 NavButtons.propTypes = {
   isFirst: PropTypes.bool.isRequired,
