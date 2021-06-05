f20c520f4b0140976bed344e1368d7d61c1533b1
andrey-dihtiarov <kama4ok222@gmail.com>
2021-06-05 19:05:24 +0300

    user-list-and-profile: UsersPage component updated

--- src/pages/users/UsersPage/index.jsx
+++ src/pages/users/UsersPage/index.jsx
@@ -1,3 +1,21 @@
-const UsersPage = () => <div>Users Page</div>;
+import { useHistory } from 'react-router-dom';
+
+import UsersTable from './UsersTable';
+
+import { users } from '../../../mocks';
+
+import styles from './styles.module.scss';
+import { ROUTES } from '../../../constants';
+
+const UsersPage = () => {
+  const history = useHistory();
+  const onUserEdit = (id) => () => history.push(ROUTES.user.replace(':id', id));
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.title}>List Of Users</div>
+      <UsersTable users={users} onUserEdit={onUserEdit} />
+    </div>
+  );
+};
 
 export default UsersPage;
