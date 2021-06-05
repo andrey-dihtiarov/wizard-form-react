f28896132a229e09961cc42da6b245db01048336
andrey-dihtiarov <kama4ok222@gmail.com>
2021-06-05 16:48:12 +0300

    user-list-and-profile: todo added

--- src/components/buttons/IconButton/index.jsx
+++ src/components/buttons/IconButton/index.jsx
@@ -2,6 +2,7 @@ import PropTypes from 'prop-types';
 
 import styles from './styles.module.scss';
 
+// TODO make button rounded
 const IconButton = ({ icon, className, children, ...props }) => (
   <button className={`${styles.button} ${className}`} {...props}>
     {icon && <span className={styles.buttonIcon}>{icon}</span>} {children}
