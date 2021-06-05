dd127f7bc31d6fb42f38f6c24e23124ecf28d08f
andrey-dihtiarov <kama4ok222@gmail.com>
2021-06-05 19:03:10 +0300

    user-list-and-profile: UserPage updated

--- src/pages/users/UserPage/index.jsx
+++ src/pages/users/UserPage/index.jsx
@@ -1,3 +1,38 @@
-const UserPage = () => <div>User Page</div>;
+import { useParams } from 'react-router-dom';
 
-export default UserPage;
+import UserProfile from './UserProfile';
+
+import { users } from '../../../mocks';
+import { ICONS, ROUTES } from '../../../constants';
+
+import LinkButton from '../../../components/buttons/LinkButton';
+import Icon from '../../../components/Icon';
+
+import styles from './styles.module.scss';
+
+const UsersPage = () => {
+  const { id } = useParams();
+  // TODO remove mocks
+  const user = users.find((u) => u.id === id);
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.inner}>
+        <div className={styles.buttonWrapper}>
+          <LinkButton
+            to={ROUTES.users}
+            className={styles.button}
+            icon={<Icon icon={ICONS.chevronLeft} className={styles.buttonIcon} />}
+          >
+            User List
+          </LinkButton>
+        </div>
+        <div className={styles.titleWrapper}>
+          <div className={styles.title}>{user.userName}</div>
+        </div>
+      </div>
+      <UserProfile user={user} />
+    </div>
+  );
+};
+
+export default UsersPage;
