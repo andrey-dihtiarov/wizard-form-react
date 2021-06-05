2a5ecc9b3aef76e08c02d994360796bb9cacd3b3
andrey-dihtiarov <kama4ok222@gmail.com>
2021-06-05 19:03:59 +0300

    user-list-and-profile: UserRow component added

---
+++ src/pages/users/UsersPage/UserRow/index.jsx
@@ -0,0 +1,96 @@
+import PropTypes from 'prop-types';
+import { formatDistanceToNow } from 'date-fns';
+
+import { ICONS } from '../../../../constants';
+
+import Avatar from '../../../../components/Avatar';
+import IconButton from '../../../../components/buttons/IconButton';
+import Icon from '../../../../components/Icon';
+
+import styles from './styles.module.scss';
+
+const UserRow = ({ user, index, selectedRow, onRowChange, onUserEdit, onUserDelete }) => {
+  const isSelected = index === selectedRow;
+  const { id, avatar, firstName, lastName, userName, company, phoneNumbers, email, lastUpdate } =
+    user;
+  const [firstPhone] = phoneNumbers;
+  return (
+    <tr className={`${styles.row} ${isSelected ? styles.rowSelected : ''}`}>
+      <td>
+        <Avatar className={styles.avatar} image={avatar} />
+      </td>
+      <td>
+        {`${firstName} ${lastName}`}
+        <div className={styles.username}>{userName}</div>
+      </td>
+      <td>{company}</td>
+      <td>{firstPhone || email}</td>
+      <td>{formatDistanceToNow(lastUpdate)} ago</td>
+      <td>
+        <IconButton
+          onClick={onUserEdit(id)}
+          icon={<Icon icon={ICONS.pen} className={styles.edit} width={12} height={12} />}
+          disabled={isSelected}
+        />
+      </td>
+      <td>
+        <IconButton
+          onClick={onRowChange(index)}
+          icon={
+            <Icon
+              icon={ICONS.times}
+              disabled={isSelected}
+              className={styles.delete}
+              width={12}
+              height={12}
+            />
+          }
+        />
+      </td>
+      {isSelected && (
+        <td>
+          <IconButton
+            icon={<Icon icon={ICONS.times} width={12} height={12} />}
+            type="button"
+            className={styles.deleteConfirm}
+            onClick={onUserDelete(id)}
+          >
+            delete
+          </IconButton>
+        </td>
+      )}
+    </tr>
+  );
+};
+
+UserRow.propTypes = {
+  user: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    avatar: PropTypes.string,
+    firstName: PropTypes.string.isRequired,
+    lastName: PropTypes.string.isRequired,
+    userName: PropTypes.string.isRequired,
+    company: PropTypes.string.isRequired,
+    phoneNumbers: PropTypes.array,
+    email: PropTypes.string.isRequired,
+    lastUpdate: PropTypes.instanceOf(Date).isRequired,
+  }),
+  onUserDelete: PropTypes.func,
+  onUserEdit: PropTypes.func,
+  onRowChange: PropTypes.func,
+  index: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+  selectedRow: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+};
+
+UserRow.defaultProps = {
+  user: {
+    avatar: null,
+    phoneNumbers: [],
+  },
+  onUserDelete: () => {},
+  onUserEdit: () => {},
+  onRowChange: () => {},
+  selectedRow: null,
+};
+
+export default UserRow;
