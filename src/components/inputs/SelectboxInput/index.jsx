e0f541faf4dd85be29797f437e694c7dceac28c2
Andrey Dihtiariov <andrey.d@giantleaplab.com>
2021-06-07 05:06:37 +0300

    index-db: selectbox setValue func added, set only value onChange

--- src/components/inputs/SelectboxInput/index.jsx
+++ src/components/inputs/SelectboxInput/index.jsx
@@ -1,16 +1,10 @@
+import { useCallback } from 'react';
 import PropTypes from 'prop-types';
 import Select from 'react-select';
-
-// import { LANGUAGES_LIST } from '../../../constants';
 
 import InputContainer from '../InputContainer';
 
 import styles from './styles.module.scss';
-
-// const selectboxList = Object.entries(LANGUAGES_LIST).map(([key, value]) => ({
-//   value: key,
-//   label: value,
-// }));
 
 const SelectboxInput = ({
   form: { touched, errors, setFieldValue, setFieldTouched },
@@ -21,6 +15,17 @@ const SelectboxInput = ({
 }) => {
   const { name, value } = field;
   const isError = !!(touched[name] && errors[name]);
+
+  const setValue = useCallback(() => {
+    if (isMulti) return valuesList.filter((opt) => value.indexOf(opt.value) >= 0);
+    return valuesList.find((opt) => opt.value === value);
+  }, [isMulti, value, valuesList]);
+
+  const onValueChange = (valList) =>
+    setFieldValue(name, isMulti ? valList.map(({ value: val }) => val) : valList.value);
+
+  const onBlur = () => setFieldTouched(name, true);
+
   return (
     <InputContainer field={field} label={label}>
       <Select
@@ -29,9 +34,9 @@ const SelectboxInput = ({
         required
         name={name}
         {...field}
-        value={value}
-        onChange={(val) => setFieldValue(name, val)}
-        onBlur={() => setFieldTouched(name, true)}
+        value={setValue()}
+        onChange={onValueChange}
+        onBlur={onBlur}
         classNamePrefix={isError ? styles.selectError : styles.select}
         className={isError ? styles.selectError : styles.select}
         isMulti={isMulti}
