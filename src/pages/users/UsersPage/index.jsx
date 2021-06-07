410de6df0b7a84199af96f0a05b430382885fc0b
Andrey Dihtiariov <andrey.d@giantleaplab.com>
2021-06-07 05:19:10 +0300

    index-db: fetchUsers added

--- src/pages/users/UsersPage/index.jsx
+++ src/pages/users/UsersPage/index.jsx
@@ -1,15 +1,23 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
+
+import { ROUTES } from '../../../constants';
+
+import { fetchUsers } from '../../../store/user';
 
 import UsersTable from './UsersTable';
 
-import { users } from '../../../mocks';
-
 import styles from './styles.module.scss';
-import { ROUTES } from '../../../constants';
 
 const UsersPage = () => {
   const history = useHistory();
   const onUserEdit = (id) => () => history.push(ROUTES.user.replace(':id', id));
+  const { users } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, [dispatch]);
   return (
     <div className={styles.wrapper}>
       <div className={styles.title}>List Of Users</div>
