9729cf1dc8b64a74b9effdb34ccb4e954530b7cc
andrey-dihtiarov <kama4ok222@gmail.com>
2021-06-08 14:45:13 +0300

    step-wizard-improvements: routes updated

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,25 +3,23 @@ import { Switch, Route, BrowserRouter } from 'react-router-dom';
 
 import { ROUTES } from '../constants';
 
-import UserFormPage from '../pages/users/UserFormPage';
+import MainLayout from '../components/layouts/MainLayout';
 import HomePage from '../pages/HomePage';
 import NotFoundPage from '../pages/NotFoundPage';
 import UserPage from '../pages/users/UserPage';
 import UsersPage from '../pages/users/UsersPage';
-import MainLayout from '../components/layouts/MainLayout';
+import EditUserPage from '../pages/users/EditUserPage';
+import NewUserPage from '../pages/users/NewUserPage';
 
 const Routes = () => (
   <BrowserRouter>
     <MainLayout>
       <Switch>
         <Route exact path={ROUTES.home} component={HomePage} />
-        <Route path={ROUTES.newUser} component={UserFormPage} />Z
+        <Route path={ROUTES.newUser} component={NewUserPage} />Z
         <Route exact path={ROUTES.user} component={UserPage} />
         <Route exact path={ROUTES.users} component={UsersPage} />
-        <Route
-          path={ROUTES.editUser}
-          component={(props) => <UserFormPage isEditing {...props} />}
-        />
+        <Route path={ROUTES.editUser} component={EditUserPage} />
         <Route component={NotFoundPage} />
       </Switch>
     </MainLayout>
