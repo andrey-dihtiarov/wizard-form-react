8bf8ed1e6d83a29baece6fd6acc723494bcec346
Andrii D <andrey.dihtiarov@gmail.com>
2021-06-11 19:03:37 +0300

    index-db: ToastContainer added

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { ToastContainer } from 'react-toastify';
+
 import Routes from './routes';
 
 import styles from './App.module.scss';
@@ -6,6 +8,7 @@ function App() {
   return (
     <div className={styles.app}>
       <Routes />
+      <ToastContainer />
     </div>
   );
 }
