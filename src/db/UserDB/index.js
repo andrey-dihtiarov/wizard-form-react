5d22a3a986a463c59b12d1e80a244a3da5587a3d
Andrii D <andrey.dihtiarov@gmail.com>
2021-06-11 19:02:59 +0300

    index-db: UserDB error handlers added

--- src/db/UserDB/index.js
+++ src/db/UserDB/index.js
@@ -5,20 +5,78 @@ class UserDB extends Database {
     super('users');
   }
 
+  getByUserName(name) {
+    return this.db[this.table].where({ userName: name }).first();
+  }
+
+  getByEmail(userEmail) {
+    return this.db[this.table].where({ email: userEmail }).first();
+  }
+
+  isEmailExists(user) {
+    return new Promise((resolve, reject) => {
+      this.getByEmail(user.email).then((data) => {
+        if (data && data.id !== user.id) {
+          reject(new Error('Email should be unique'));
+        }
+        resolve();
+      });
+    });
+  }
+
+  isUserNameExists(user) {
+    return new Promise((resolve, reject) => {
+      this.getByUserName(user.userName).then((data) => {
+        if (data && data.id !== user.id) {
+          reject(new Error('User Name should be unique'));
+        }
+        resolve();
+      });
+    });
+  }
+
+  isUserExists(userId) {
+    return this.db[this.table]
+      .where({ id: userId })
+      .first()
+      .then((data) => data)
+      .catch(() => new Error('User is now found'));
+  }
+
   addUser(user) {
-    return this.add(user);
+    return this.isUserNameExists(user)
+      .then(() =>
+        this.isUserNameExists(user)
+          .then(() => this.add(user))
+          .catch((e) => ({ error: { field: 'userName', message: e.message } })),
+      )
+      .catch((e) => ({ error: { field: 'email', message: e.message } }));
   }
 
   updateUser(user, id) {
-    return this.put(user, id);
+    return this.isUserExists(id)
+      .then(() =>
+        this.isUserNameExists(user)
+          .then(() =>
+            this.isUserNameExists(user)
+              .then(() => this.put(user, id))
+              .catch((e) => ({ error: { field: 'email', message: e.message } })),
+          )
+          .catch((e) => ({ error: { field: 'userName', message: e.message } })),
+      )
+      .catch((e) => ({ error: { field: 'id', message: e.message } }));
   }
 
   deleteUser(id) {
-    return this.delete(id);
+    return this.isUserExists(id)
+      .then(() => this.delete(id))
+      .catch((e) => ({ error: { field: 'id', message: e.message } }));
   }
 
   getUser(id) {
-    return this.getByID(id);
+    return this.isUserExists(id)
+      .then(() => this.getByID(id))
+      .catch((e) => ({ error: { field: 'id', message: e.message } }));
   }
 
   getUsers() {
