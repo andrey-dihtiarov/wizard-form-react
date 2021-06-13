f961a7295355d460538689b9021657222b71288d
andrey-dihtiarov <kama4ok222@gmail.com>
2021-06-13 19:00:50 +0300

    extras: Search added to UsersPage

--- src/pages/users/UsersPage/index.jsx
+++ src/pages/users/UsersPage/index.jsx
@@ -1,16 +1,17 @@
-import { useEffect } from 'react';
+import { useCallback, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 
 import { ROUTES } from '../../../constants';
 
-import { deleteUser, fetchUsers, generateUsers } from '../../../store/user';
+import { deleteUser, fetchUsers, generateUsers, searchUsers } from '../../../store/user';
 
 import styles from './styles.module.scss';
 
 import UsersTable from './UsersTable';
 import PageLayout from '../../../components/layouts/PageLayout';
 import FlatButton from '../../../components/buttons/FlatButton';
+import Search from '../../../components/Search';
 
 const UsersPage = () => {
   const history = useHistory();
@@ -29,10 +30,22 @@ const UsersPage = () => {
 
   useEffect(() => {
     dispatch(fetchUsers());
-  }, [dispatch, users]);
+  }, [dispatch]);
+
+  const onSearch = useCallback(
+    (query) => {
+      dispatch(searchUsers(query));
+    },
+    [dispatch],
+  );
 
   return (
     <PageLayout title="List of users">
+      <Search
+        onSearch={onSearch}
+        className={styles.search}
+        placeholder="Search users by first name or last name"
+      />
       <UsersTable users={users} onUserEdit={onUserEdit} onUserDelete={onUserDelete} />
       <div className={styles.buttonWrapper}>
         <FlatButton onClick={onGenerateClick}>Generate Users</FlatButton>
