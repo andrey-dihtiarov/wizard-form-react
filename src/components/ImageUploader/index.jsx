40f8a34f7135079ddc2c39f3f1840e87fdb59616
Andrii D <andrey.dihtiarov@gmail.com>
2021-06-14 16:07:46 +0300

    extras: ImageUploader updated for image cropping

--- src/components/ImageUploader/index.jsx
+++ src/components/ImageUploader/index.jsx
@@ -1,12 +1,14 @@
-import React, { useRef } from 'react';
+import { useRef, useState, useCallback } from 'react';
 import PropTypes from 'prop-types';
 
 import { ICONS } from '../../constants';
 
 import IconButton from '../buttons/IconButton';
 
-import styles from './styles.module.scss';
 import InputContainer from '../inputs/InputContainer';
+import CropModal from './CropModal';
+
+import styles from './styles.module.scss';
 
 export function ImageUploader({
   label,
@@ -16,6 +18,12 @@ export function ImageUploader({
   form: { errors, setFieldValue, setFieldTouched },
   ...props
 }) {
+  const [photo, setPhoto] = useState(null);
+  const [photoBase64, setPhotoBase64] = useState(null);
+  const [image, setImage] = useState();
+
+  const [isCropperVisible, setIsCropperVisible] = useState(false);
+
   const fileInputEl = useRef();
   const { name } = field;
 
@@ -28,28 +36,57 @@ export function ImageUploader({
   async function readFile(file) {
     return new Promise((resolve) => {
       const reader = new FileReader();
-      reader.onload = (e) => {
+      reader.onloadend = (e) => {
         resolve(e.target.result);
       };
       reader.readAsDataURL(file);
     });
   }
 
-  async function onFileChange(e) {
-    const file = e.target.files[0];
-    if (file) {
-      const img = await readFile(file);
-      setFieldValue(name, img);
-      setFieldTouched(name, true, true);
+  const onFileChange = useCallback(
+    (event) => {
+      event.preventDefault();
+      const [file] = event.target.files;
+      const reader = new FileReader();
+
+      reader.onloadend = () => {
+        setPhoto(file);
+        setPhotoBase64(reader.result);
+        setFieldValue(name, file);
+        setFieldTouched(name, true, true);
+        setIsCropperVisible(true);
+      };
+
+      reader.readAsDataURL(file);
+    },
+    [name, setFieldTouched, setFieldValue],
+  );
+
+  const updateCroppedImageFile = useCallback(
+    async (file) => {
       if (errors[name]) {
+        setIsCropperVisible(false);
         return;
       }
+      setPhoto(file);
+      const img = await readFile(file);
+      setFieldValue(name, img);
+      setIsCropperVisible(false);
       onChange(img);
-    }
-  }
+    },
+    [errors, name, onChange, setFieldValue],
+  );
 
   return (
     <InputContainer className={className} field={field}>
+      <div className={styles.imageWrapper}>
+        <img
+          ref={(imageRef) => setImage(imageRef)}
+          src={photo && URL.createObjectURL(photo)}
+          alt={photo}
+          className={styles.image}
+        />
+      </div>
       <input
         {...field}
         {...props}
@@ -61,9 +98,20 @@ export function ImageUploader({
         accept=".jpg,.jpeg,.png,gif,.svg"
         value=""
       />
-      <IconButton type="button" icon={ICONS.add} onClick={onFileChangePopup}>
+      <IconButton
+        type="button"
+        icon={ICONS.add}
+        onClick={onFileChangePopup}
+        className={styles.addButton}
+      >
         {label}
       </IconButton>
+      <CropModal
+        image={image}
+        imageBase64={photoBase64}
+        updateImageFile={updateCroppedImageFile}
+        isVisible={isCropperVisible}
+      />
     </InputContainer>
   );
 }
