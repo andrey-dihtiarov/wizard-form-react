33b419320d471db83fcbf8913effafca30326228
Andrii D <andrey.dihtiarov@gmail.com>
2021-06-14 16:06:48 +0300

    extras: CropModal component added

---
+++ src/components/ImageUploader/CropModal/index.jsx
@@ -0,0 +1,80 @@
+import React, { useCallback, useRef, useState } from 'react';
+import Cropper from 'react-easy-crop';
+
+import FlatButton from '../../buttons/FlatButton';
+
+import styles from './styles.module.scss';
+
+const CropModal = ({ updateImageFile, image, imageBase64, isVisible }) => {
+  const [crop, setCrop] = useState({ x: 0, y: 0 });
+  const [zoom, setZoom] = useState(1);
+  const [croppedAreaPixelsState, setCroppedAreaPixelsState] = useState({
+    width: 1,
+    height: 1,
+  });
+
+  const canvasRef = useRef(null);
+  const ctx = canvasRef.current ? canvasRef.current.getContext('2d') : null;
+
+  const onCropComplete = useCallback(
+    (croppedArea, croppedAreaPixels) => {
+      if (image) {
+        setCroppedAreaPixelsState(croppedAreaPixels);
+
+        const {
+          current: { width, height },
+        } = canvasRef;
+
+        ctx.clearRect(0, 0, width || 0, height || 0);
+
+        ctx.drawImage(
+          image,
+          croppedAreaPixels.x,
+          croppedAreaPixels.y,
+          croppedAreaPixels.width,
+          croppedAreaPixels.height,
+          0,
+          0,
+          croppedAreaPixels.width,
+          croppedAreaPixels.height,
+        );
+      }
+    },
+    [ctx, image],
+  );
+
+  const onCropSubmit = useCallback(() => {
+    if (canvasRef.current) {
+      canvasRef.current.toBlob((file) => {
+        updateImageFile(file);
+      }, 'image/jpg');
+    }
+  }, [updateImageFile]);
+
+  return (
+    <div className={`${styles.cropWrapper} ${isVisible ? styles.cropWrapperVisible : ''}`}>
+      <div className={styles.buttonWrapper}>
+        <FlatButton type="button" onClick={onCropSubmit}>
+          Crop
+        </FlatButton>
+      </div>
+      <canvas
+        ref={canvasRef}
+        width={croppedAreaPixelsState.width}
+        height={croppedAreaPixelsState.height}
+      />
+      <Cropper
+        image={imageBase64}
+        crop={crop}
+        zoom={zoom}
+        aspect={1}
+        onCropChange={setCrop}
+        onCropComplete={onCropComplete}
+        onZoomChange={setZoom}
+        cropShape="round"
+      />
+    </div>
+  );
+};
+
+export default CropModal;
