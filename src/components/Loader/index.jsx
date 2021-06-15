efd9056a8a02c73e9a22e6b194a8d6823a4b3c01
Andrii D <andrey.dihtiarov@gmail.com>
2021-06-15 17:48:56 +0300

    extras: removed unnecessary code from Loader component

--- src/components/Loader/index.jsx
+++ src/components/Loader/index.jsx
@@ -1,27 +1,12 @@
-import PropTypes from 'prop-types';
-
 import styles from './styles.module.scss';
 
-const Loader = ({ withOverlay, isFixed }) => (
-  <div>
-    {withOverlay && <div className={styles.overlay} />}
-    <div className={`${styles.ldsEllipsis} ${isFixed ? styles.ldsEllipsisFixed : ''}`}>
-      <div />
-      <div />
-      <div />
-      <div />
-    </div>
+const Loader = () => (
+  <div className={styles.ldsEllipsis}>
+    <div />
+    <div />
+    <div />
+    <div />
   </div>
 );
-
-Loader.propTypes = {
-  withOverlay: PropTypes.bool,
-  isFixed: PropTypes.bool,
-};
-
-Loader.defaultProps = {
-  withOverlay: false,
-  isFixed: true,
-};
 
 export default Loader;
