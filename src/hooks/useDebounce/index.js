493ba3f79aff20e97f3777879ab8dc89389d3c2c
Andrii D <andrey.dihtiarov@gmail.com>
2021-06-16 14:32:30 +0300

    extras: default param added

--- src/hooks/useDebounce/index.js
+++ src/hooks/useDebounce/index.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 
-export default (value, timeout) => {
+export default (value, timeout = 500) => {
   const [state, setState] = useState(value);
 
   useEffect(() => {
