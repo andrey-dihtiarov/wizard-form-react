b19bbe00d4f810e162c80efd9211e7c3a3e4e5ae
andriid <andrii.dikhtiarov@brocoders.team>
2021-06-23 17:16:57 +0300

    material-ui: StylesProvider added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { StylesProvider } from '@material-ui/core/styles';
 
 import reportWebVitals from './reportWebVitals';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
@@ -14,7 +15,9 @@ import './index.scss';
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <StylesProvider injectFirst>
+        <App />
+      </StylesProvider>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
