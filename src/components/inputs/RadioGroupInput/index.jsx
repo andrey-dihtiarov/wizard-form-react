595a7567f0320fdf9cc405e5a838aaf7f86f7583
andriid <andrii.dikhtiarov@brocoders.team>
2021-06-25 11:50:19 +0300

    material-ui: RadioGroup changed to material ui

--- src/components/inputs/RadioGroupInput/index.jsx
+++ src/components/inputs/RadioGroupInput/index.jsx
@@ -1,26 +1,39 @@
 import PropTypes from 'prop-types';
-import { Field } from 'formik';
+import { Radio, RadioGroup, FormControlLabel } from '@material-ui/core';
 
 import InputContainer from '../InputContainer';
 
 import styles from './styles.module.scss';
 
-const RadioGroupInput = ({ values, name }) => (
-  <InputContainer field={{ name }} label="Gender">
-    <div className={styles.radioGroup}>
-      {values.map((value) => (
-        <label htmlFor={value} key={value}>
-          <Field type="radio" name={name} value={value} id={value} />
-          {value}
-        </label>
-      ))}
-    </div>
-  </InputContainer>
-);
+const RadioGroupInput = ({ values, field, form: { setFieldValue, setFieldTouched } }) => {
+  const { name, value } = field;
+
+  const onChange = ({ target: { value: v } }) => {
+    setFieldValue(name, v);
+    setFieldTouched(name, true);
+  };
+
+  return (
+    <InputContainer field={field}>
+      <RadioGroup name={name} value={value} onChange={onChange} className={styles.radioGroup}>
+        {values.map((v) => (
+          <FormControlLabel value={v} key={v} label={v} control={<Radio color="primary" />} />
+        ))}
+      </RadioGroup>
+    </InputContainer>
+  );
+};
 
 RadioGroupInput.propTypes = {
   values: PropTypes.array.isRequired,
-  name: PropTypes.string.isRequired,
+  field: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    value: PropTypes.string,
+  }).isRequired,
+  form: PropTypes.shape({
+    touched: PropTypes.object,
+    errors: PropTypes.object,
+  }).isRequired,
 };
 
 export default RadioGroupInput;
